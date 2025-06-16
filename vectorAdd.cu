7b61e3b11e3a46cc5b07429435fcfb07f4a18c96
Ananth Junutula <ananthkrj@gmail.com>
2025-06-15 23:30:22 -0700

    First day, basic kernel, thread, and block info learned

---
+++ vectorAdd.cu
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cuda_runtime.h>
+
+// To be finished
+// vector addition
+__global__ void VecAdd(float* A, float* B, float* C)
+{
+    int i = threadIdx.x;
+    C[i] = A[i] + B[i];
+}
+
+int main()
+{
+    VecAdd<<<1, N>>>(A, B, C);
+}
