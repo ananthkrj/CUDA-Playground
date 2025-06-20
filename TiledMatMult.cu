93fe26b2e79bf0af0574d717f73e611b42151b4c
Ananth Junutula <ananthkrj@gmail.com>
2025-06-19 18:38:04 -0700

    Finished

--- TiledMatMult.cu
+++ TiledMatMult.cu
@@ -5,17 +5,22 @@
 // need to define width of a tile as
 // a constant
 #define TILE_WIDTH 16
-// Potentially utilize a struct? Likely dont
-// need it, but good review
+
+// helper to initialize the matrix
+void initMatrix(float* mat, int N) {
+    for(int i = 0; i < N * N; i++) {
+        mat[i] = static_cast<float>(rand() % 10);
+    }
+}
 
 
 // Split into 3 different functions
 
-// The device Helper
+// The device Helper for flat 2d access
 __device float getElement(float* matrix, int row, int col, int width) {
     // Assume matrix is a flat 1D array representing a 2D
     // matrix
-    return M[row * N + col];
+    return matrix[row * width + col];
 }
 
 // The actual matmult kernel
@@ -45,26 +50,26 @@ __global__ void TiledMatMult(float* A, float* B, float* C, int N) {
         // load from global memory into shared
         // using  get element
         // threading row and column as threadIdx.y and threadIdx.x
-        Asub[threadIdx.y][threadIdx.x] = getElement(matrix, row, col calculation, N);
-        Bsub[threadIdx.y][threadIdx.x] = getElement(matrix, row calculation, col, N);
+        Asub[threadIdx.y][threadIdx.x] = getElement(A, row, i * TILE_WIDTH + threadIdx.x, N);
+        Bsub[threadIdx.y][threadIdx.x] = getElement(B, i * TILE_WIDTH + threadIdx.y, col, N);
         // then we synchronize to make sure sub matrixes are 
         // loaded before computation
         __syncthreads();
-        // now multiply the submatrices
-        // and populate the acc variable with the value
+        
+        // now multiply the submatrices, performing 
+        // partial multiplication
         for (int j = 0; j < TILE_WIDTH; j++) {
             // multiply row of sub matrix A and column of submatrix B
-            acc += Asub[threadIdx.y][j] * Bsub[i][threadIdx.x];
-
-            // synchronize before loading new submatrices
-            __synchthreads();
+            acc += Asub[threadIdx.y][j] * Bsub[j][threadIdx.x];
         }
+        // synchronize before loading new submatrices
+        __synchthreads();
     }
+
     // populate C with value of acc
     if (row < N && col < N) {
         C[row * N + col] = acc;
     }
-
 }
 
 // function to launch matmult kernel
@@ -73,27 +78,69 @@ void launchTiledMatMult(float* h_A, float h_B, float h_C, int N) {
     size_t size = N * N * sizeof(float);
 
     // initialize device variables for matrices
+    float* d_A;
+    // alocate using pass by address
+    cudaMalloc(&d_A, size);
+    float* d_B;
+    cudaMalloc(&d_B, size);
+    float* d_C;
+    cudaMalloc(&d_C, size);
 
     // copy from host to device
+    cudaMemcpy(d_A, h_A, size, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_B, h_B, size, cudaMemcpyHostToDevice);
 
     // configure grid and block dimensions
+    // these dimensions are the constant TILE_WIDTH
+    dim3 dimBlock(TILE_WIDTH, TILE_WIDTH);
+    dim3 dimGrid((N + TILE_WIDTH - 1) / TILE_WIDTH, (N + TILE_WIDTH - 1) / TILE_WIDTH);
     
     // launch the kernel
+    TiledMatMult<<<dimGrid, dimBlock>>>(d_A, d_B, d_C, N)
+
+    // add error checking later
 
     // copy back from device to host, but only for product (matrix C)
+    cudaMemcpy(h_C, d_C, size, cudaMemcpyDeviceToHost);
 
     // free the device memory
+    cudaFree(d_A);
+    cudaFree(d_B);
+    cudaFree(d_C);
 }
 
 int main() {
     // intialize N
     // initialize size again
+    // should be multiple of 16
+    int N = 256 
+    size_t size = N * N * sizeof(float);
 
     // allocate host memory
+    float* h_A = (float*)malloc(size);
+    float* h_B = (float*)malloc(size);
+    float* h_C = (float*)malloc(size);
 
     // initialize host A and B, using initMatrix
+    initMatrix(h_A, N);
+    initMatrix(h_B, N);
 
     // launchTIledMatmUlt (all device ops there)
+    launchTiledMatMult(h_A, h_B, h_C, N);
 
     // printing result to verify 
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; J < N; j++) {
+            std::cout << h_C[i * N + j];
+        }
+        std::cout << '\n';
+    }
+
+
+    // free host memory
+    free(h_A);
+    free(h_B);
+    free(h_C);
+
+    return 0;
 }
